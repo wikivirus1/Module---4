7c12c088449bd85f87b1b4688848b54d8c31d5da
KEVINBHIMANI1 <91408173+KEVINBHIMANI1@users.noreply.github.com>
2021-10-31 21:25:24 -0700

    Delete Goodbye.js

--- Goodbye.js
+++
@@ -1,9 +0,0 @@
-// Goodbye
-(function(window) {
-    var byeSpeaker = new Object();
-    var speakWord = "Good Bye";
-    byeSpeaker.speak = function speak(name) {
-        console.log(speakWord + " " + name);
-    };
-    window.byeSpeaker = byeSpeaker;
-})(window)
